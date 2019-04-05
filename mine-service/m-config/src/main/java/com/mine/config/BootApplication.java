949cdef2006624042b7102ed142f5558ded10c2b
zhoutianmao <xczhenhk@163.com>
2019-04-05 11:19:39 +0800

    初始化配置中心

---
+++ mine-service/m-config/src/main/java/com/mine/config/BootApplication.java
@@ -0,0 +1,9 @@
+package com.mine.config;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class BootApplication {
+}
